6d2494414360e6791d9bb8abea099838e96d9d74
MWatson04 <91926425+MWatson04@users.noreply.github.com>
2024-09-10 14:12:31 -0500

    Created character file to hold all character class information

---
+++ character.py
@@ -0,0 +1,36 @@
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-class-docstring
+# pylint: disable=missing-function-docstring
+
+class Human:
+    def __init__(self, name = "Balder", damage = 75, health = 150):
+        self.name = name
+        self.damage = damage
+        self.health = health
+
+    def attack(self):
+        print(f"{self.name} did {self.damage} damage!")
+
+    def take_damage(self, damage_taken):
+        self.health -= damage_taken
+        self.health = max(self.health, 0)
+        
+        print(f"{self.name} has taken {damage_taken} damage")
+        print(f"{self.name} health is now {self.health}")
+        print("")
+
+class Elf(Human):
+    def __init__(self, name = "Solana", damage = 85, health = 100):
+        super().__init__(name, damage, health)
+
+class Orc(Human):
+    def __init__(self, name = "Ugor", damage = 65, health = 175):
+        super().__init__(name, damage, health)
+
+class Wizard(Human):
+    def __init__(self, name = "Iric", damage = 75, health = 125):
+        super().__init__(name, damage, health)
+
+class Dragon(Human):
+    def __init__(self, name = "Alduin", damage = 25, health = 400):
+        super().__init__(name, damage, health)
