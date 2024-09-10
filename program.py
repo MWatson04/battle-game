3cca32292f254ce6af93496029ac903a3191a54c
MWatson04 <91926425+MWatson04@users.noreply.github.com>
2024-09-10 10:46:59 -0500

    Setup game class with initial game setup logic

--- program.py
+++ program.py
@@ -32,6 +32,56 @@ class Dragon(Human):
     def __init__(self, name = "Alduin", damage = 25, health = 400):
         super().__init__(name, damage, health)
 
-dragon_obj = Dragon()
+class Game:
+    def __init__(self, game_over = False, state_one_over = False, chosen_class = None):
+        self.game_over = game_over
+        self.state_one_over = state_one_over
+        self.chosen_class = chosen_class
 
-dragon_obj.attack()
+    def game_setup(self):
+        valid_choice = False
+        print("Welcome to Battle Game")
+
+        while not valid_choice:
+            print("")
+            print("Please select your class to start")
+            print("1)   Human")
+            print("2)   Elf")
+            print("3)   Orc")
+            print("4)   Wizard")
+
+            class_input = input("Enter the number or name of a class to make your choice: ").lower()
+
+            if class_input in ("1", "human"):
+                self.chosen_class = Human()
+                print(f"You have chosen the human class. Your name is {self.chosen_class.name}!")
+                print(f"Your Health is: {self.chosen_class.health}")
+                print(f"Your Attack Damage is: {self.chosen_class.damage}")
+                valid_choice = True
+            elif class_input in ("2", "elf"):
+                self.chosen_class = Elf()
+                print(f"You have chosen the elf class. Your name is {self.chosen_class.name}!")
+                print(f"Your Health is: {self.chosen_class.health}")
+                print(f"Your Attack Damage is: {self.chosen_class.damage}")
+                valid_choice = True
+            elif class_input in ("3", "orc"):
+                self.chosen_class = Orc()
+                print(f"You have chosen the orc class. Your name is {self.chosen_class.name}!")
+                print(f"Your Health is: {self.chosen_class.health}")
+                print(f"Your Attack Damage is: {self.chosen_class.damage}")
+                valid_choice = True
+            elif class_input in ("4", "wizard"):
+                self.chosen_class = Wizard()
+                print(f"You have chosen the wizard class. Your name is {self.chosen_class.name}!")
+                print(f"Your Health is: {self.chosen_class.health}")
+                print(f"Your Attack Damage is: {self.chosen_class.damage}")
+                valid_choice = True
+            else:
+                print("Invalid choice. Choose again")
+
+def run_game():
+    game_obj = Game()
+
+    game_obj.game_setup()
+
+run_game()
